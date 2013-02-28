b7f72f80e5838f8b3dfcc49ead031146011cff8e
Oleksandr Petrov <oleksandr.petrov@gmail.com>
2013-02-28 23:46:26 +0100

    Updating layout, adding affix & scroll spies, improving readability.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,6 @@
 source :rubygems
 
 gem "jekyll"
-gem "rdiscount"
+gem "redcarpet"
 gem "haml"
 gem "RedCloth"
--- assets/javascripts/toc.js
+++ assets/javascripts/toc.js
@@ -1,50 +1,84 @@
-jQuery.fn.toc = function () {
-  if(this.length === 0)
-    return;
+// jQuery.fn.toc = function () {
+//   if(this.length === 0)
+//     return;
 
-  var listStack = [ $("<ul class='nav nav-list' />")];
-  listStack[0].appendTo(this);
+//   var listStack = [$("<ul class='nav nav-list bs-docs-sidenav'/>")];
+//   listStack[0].appendTo(this);
 
-  Array.prototype.last = function() { return this[this.length - 1]};
+//   Array.prototype.last = function() { return this[this.length - 1]};
 
-  var level = 2;
-  $(document).ready(function() {
-    $(":header").each(function(index, el) {
+//   var level = 2;
+//   $(document).ready(function() {
+//     $(":header").each(function(index, el) {
+//       var currentLevel = 2;
 
-      if(parseInt(el.tagName[1]) === 1)
-        return;
+//       var text = $(el).text();
+//       var anchor = text.replace(/[^a-zA-Z 0-9]+/g,'').replace(/\s/g, "_").toLowerCase();
 
-      var text = $(el).text();
+//       $(el).attr('id', anchor);
 
-      var anchor = text.replace(/[^a-zA-Z 0-9]+/g,'').replace(/\s/g, "_").toLowerCase();
-      $(el).attr('id', anchor);
+//       if(currentLevel > level) {
+//         var nextLevelList = $("<ul class='nav nav-list'/>");
+//         nextLevelList.appendTo(listStack.last().children("li").last());
+//         listStack.push(nextLevelList);
+//       } else if(currentLevel < level) {
+// 	      var delta = level - currentLevel;
+//         for(var i = 0; i < delta; i ++) {
+// 	        listStack.pop();
+// 	      }
+//       }
 
-      var currentLevel = parseInt(el.tagName[1]);
+//       level = currentLevel;
+//       var li = $("<li />");
 
-      if(currentLevel > level) {
-        var nextLevelList = $("<ul class='nav nav-list'/>");
-        nextLevelList.appendTo(listStack.last().children("li").last());
-        listStack.push(nextLevelList);
-      } else if(currentLevel < level) {
-        listStack.pop();
-      }
+//       $("<a />").text(text).attr('href', "#" + anchor).appendTo(li);
+//       li.appendTo(listStack.last());
+//     });
+//   });
+// };
 
-      level = currentLevel;
-      var li = $("<li />");
+// $(document).ready(function() {
+//   $("#side-navigation").toc();
 
-      $("<a />").text(text).attr('href', "#" + anchor).appendTo(li);
-      li.appendTo(listStack.last());
-    });
-  });
-};
-// $($(".highlight")[2]).text().match(/(.*)\.(.*)/)
-// "AMQP::Channel.direct".match(/(.*)\.(.*)/)
-// "AMQP::Channel#direct".match(/(.*)#(.*)/)
-jQuery.fn.yardLink = function () {
-  var class_method = /(.*)\.(.*)/;
-  var instance_method = /(.*)#(.*)/;
-};
+//   $("#side-navigation .nav-list").affix({
+//     offset: {
+//       top: 0,
+//       bottom: 0
+//     }
+//   });
+
+//     $('#side-navigation .nav-list').on('affixed', function () {
+//       console.log(123123)
+//         $('#side-navigation .nav-list').addClass('navbar-fixed-top')
+//     });
+
+//     $('#side-navigation .nav-list').on('unaffixed', function () {
+//         $('#side-navigation .nav-list').removeClass('navbar-fixed-top')
+//     });
+// });
 
 $(document).ready(function() {
-  $(".well.sidebar-nav").toc();
+  var $toc = $('#toc');
+
+  function format (title) {
+    var $title = $(title),
+        txt = $title.text(),
+        id = $title.attr('id');
+    return "<li> <a href=#" + id + ">" + txt + "</a></li>";
+  }
+  // return;
+
+  if($toc.length) {
+    var $h3s = $('.span9 :header');
+    var titles = $h3s.map(function () {
+      return format(this);
+    }).get().join('');
+    $toc.html(titles);
+  }
+
+  $("#toc").affix();
+  // $('[data-spy="scroll"]').each(function () {
+    // var $spy = $(this).scrollspy('refresh');
+  // });
+
 });
